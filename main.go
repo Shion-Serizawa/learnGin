985a2faeb7a5584eb0de042b2548c9f47e4b152f
seri. <anagi4699@gmail.com>
2025-01-01 17:55:49 +0900

    🔧POSTの雰囲気を学んだ

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
@@ -11,4 +17,31 @@ var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+
+	router.Run("localhost:8080")
+}
+
+// getAlbumsは全てのアルバムをJSONとしてレスポンスします。
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// postAlbumsはリクエストボディで受け取ったJSONからアルバムを追加します。
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// BindJSONを呼び出してリクエストボディをnewAlbumにバインドします。
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// 新しいアルバムをスライスに追加します。
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
